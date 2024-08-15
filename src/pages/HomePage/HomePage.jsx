9837598cfab6702eb117651c5a2a55a1e9adf8ec
Gerald-Ivan <148294223+Gerald-Ivan@users.noreply.github.com>
2024-08-15 15:10:31 +0800

    update moviepage and homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import { fetchTrendingMovies } from 'MoviesApi';
+import { useEffect, useState } from 'react';
+import { MovieList } from 'components/MovieList/MovieList';
 
 export const HomePage = () => {
+  const [trendingMovies, setTrendingMovies] = useState([]);
+
+
+  useEffect(() => {
+    
+  const loadTrendingMovies = async () =>{
+    try {
+      const movies = await fetchTrendingMovies();
+      setTrendingMovies(movies);
+    } catch (error) {
+      console.log(error)
+    }
+  };
+  
+    loadTrendingMovies();
+  }, []);
+
+
   return (
     <div> 
         <h2>Trending Today</h2>
+        {trendingMovies.length && <MovieList movies={trendingMovies}/>}
     </div>
   )
 }
--- src/pages/MoviePage/MoviePage.jsx
+++ src/pages/MoviePage/MoviePage.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { MovieList } from 'components/MovieList/MovieList';
+import { fetchMovieByQuery } from 'MoviesApi';
 
 export const MoviePage = () => {
+  const [searchQuery, setSearchQuery] = useState ('');
+  const [movies, SetMovies] = useState ([]);
+
+    const fetchMovies = async () =>{
+      if (!searchQuery.trim()) return;
+      try {
+        const movies = await fetchMovieByQuery();
+        console.log('movies', movies)
+        SetMovies(movies);
+      } catch (error) {
+        console.log(error)
+      }
+    };
+    
+
   return (
     <div>
-      <input placeholder='Search Movies'/>
-      <button>Search</button>
-      <h2>Movie</h2>
+      <div>
+        <input onChange={e => setSearchQuery(e.target)} placeholder='Search Movies...'/>
+        <button onClick={fetchMovies}/>        
+      </div>
+      <MovieList movies={movies}/>
     </div>
   )
 }
